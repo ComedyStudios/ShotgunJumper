9fc4c4ed816ac001fe281f7760a2a85ebeb1b7f2
Mischa Prudnikov <mishaprudnikov11@gmail.com>
2022-06-14 23:10:26 +0200

    tried adding tiles from prefab with no succsess

---
+++ MicroGameJam/Assets/scripts/GameMechanics/MapGeneration.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.Tilemaps;
+
+namespace GameMechanics
+{
+    public class MapGeneration : MonoBehaviour
+    {
+        public Tilemap tiles ;
+        public RuleTile tile ;
+        public Vector3Int location ;
+        public InputAction MouseDownAction;
+        public Tilemap tilemap;
+        public BoundsInt area;
+
+        private void OnEnable()
+        {
+            MouseDownAction.Enable();
+        }
+
+        private void OnDisable()
+        {
+            MouseDownAction.Disable();
+        }
+
+        private void Start()
+        {
+            MouseDownAction.performed += _ => SetTiles();
+        }
+
+        private void SetTiles()
+        {
+            Vector3 mp = Camera.main.ScreenToWorldPoint(Mouse.current.position.ReadValue());
+            location = tiles.WorldToCell(mp);
+            tiles.SetTile(location, tile);
+            Debug.Log(location);
+
+            var tilesArray = tilemap.GetTilesBlock(tilemap.cellBounds);
+            tilemap.SetTilesBlock(area, tilesArray);
+        }
+    }
+}
