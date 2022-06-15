c7db5f44f2937ffe9fb860097bb9660adfe2bd85
ComedyStudios <mishaprudnikov11@gmail.com>
2022-06-15 16:43:55 +0200

    did some map generation shit

--- MicroGameJam/Assets/scripts/GameMechanics/MapGeneration.cs
+++ MicroGameJam/Assets/scripts/GameMechanics/MapGeneration.cs
@@ -1,45 +1,69 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Text;
 using Unity.VisualScripting;
 using UnityEngine;
-using UnityEngine.InputSystem;
+using UnityEngine.Serialization;
 using UnityEngine.Tilemaps;
+using Random = UnityEngine.Random;
+
 
 namespace GameMechanics
 {
     public class MapGeneration : MonoBehaviour
     {
-        public Tilemap tiles ;
-        public RuleTile tile ;
-        public Vector3Int location ;
-        public InputAction MouseDownAction;
-        public Tilemap tilemap;
-        public BoundsInt area;
-
-        private void OnEnable()
-        {
-            MouseDownAction.Enable();
-        }
-
-        private void OnDisable()
-        {
-            MouseDownAction.Disable();
-        }
+        public Tilemap mainTilemap ;
+        public List<Tilemap> tilemaps;
+        public List<Tilemap> conectors;
+        public int rooms;
 
         private void Start()
         {
-            MouseDownAction.performed += _ => SetTiles();
+            //InvokeRepeating(nameof(SetTiles), 0f, 1f);
+            GenerateDungeon();
         }
 
         private void SetTiles()
         {
-            Vector3 mp = Camera.main.ScreenToWorldPoint(Mouse.current.position.ReadValue());
-            location = tiles.WorldToCell(mp);
-            tiles.SetTile(location, tile);
-            Debug.Log(location);
+            var map = Random.Range(0, tilemaps.Count-1);
+            mainTilemap.ClearAllTiles();
+            for (int i = 0; i < rooms; i++)
+            {
+                var location = new Vector3Int(0,0);
+                tilemaps[map].CompressBounds();
+                var tilesArray = tilemaps[map].GetTilesBlock(tilemaps[map].cellBounds);
+                mainTilemap.SetTilesBlock(new BoundsInt(location, tilemaps[map].size), tilesArray);
+            }
+        }
 
-            var tilesArray = tilemap.GetTilesBlock(tilemap.cellBounds);
-            tilemap.SetTilesBlock(area, tilesArray);
+        private void GenerateDungeon()
+        {
+            int[,] dungeon = new int[4, 4];
+
+            var startTile = new Vector2(Random.Range(1, dungeon.GetLength(0)), Random.Range(1, dungeon.GetLength(0)));
+            /*for (int x = 0;x<dungeon.GetLength(0);x++)
+            {
+                for (int y = 0; y<dungeon.GetLength(1);y++ )
+                {
+                    dungeon[x, y] = Random.Range(0, 2);
+                }
+            }*/
+            PrintArray(dungeon);
+        }
+
+        private void PrintArray(int[,] floorMapArray)
+        {
+            StringBuilder sb = new StringBuilder();
+            for(int i=0; i< floorMapArray.GetLength(0); i++)
+            {
+                for(int j=0; j<floorMapArray.GetLength(1); j++)
+                {
+                    sb.Append(floorMapArray [i,j]);
+                    sb.Append(' ');				   
+                }
+                sb.AppendLine();
+            }
+            Debug.Log(sb.ToString());
         }
     }
 }
